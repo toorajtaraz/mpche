4a0d40ede7e631029eb8e8df6ed36c86b390165c
Tooraj Taraz <tooraj.info@gmail.com>
2022-02-11 18:58:25 +0330

    correcting prints and infos

--- mpche.cpp
+++ mpche.cpp
@@ -2,14 +2,13 @@
 #include <omp.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
-int main(int argc, char** argv )
-{
+int main(int argc, char** argv ) {
     #pragma omp parallel
     {
         printf("Hello World... from thread = %d\n",
             omp_get_thread_num());
     }
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World from non parallel part!\n";
     waitKey(0);
     return 0;
 }
