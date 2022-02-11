6b122633146cc63146951fd32394c1dfeaba3b64
Tooraj Taraz <tooraj.info@gmail.com>
2022-02-11 17:57:42 +0330

    adding base for openmp

--- mpche.cpp
+++ mpche.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <omp.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
 int main(int argc, char** argv )
 {
+    #pragma omp parallel
+    {
+        printf("Hello World... from thread = %d\n",
+            omp_get_thread_num());
+    }
     std::cout << "Hello, World!\n";
     waitKey(0);
     return 0;
