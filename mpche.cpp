afc8343f382daed55037a2e85fb99cabef3db765
Tooraj Taraz <tooraj.info@gmail.com>
2022-03-28 19:22:58 +0430

    adding sections for each dev

--- mpche.cpp
+++ mpche.cpp
@@ -9,6 +9,17 @@ int main(int argc, char** argv ) {
             omp_get_thread_num());
     }
     std::cout << "Hello, World from non parallel part!\n";
+    /*TOORAJ BEGIN*/
+
+    /*TOORAJ END*/
+
+    /*ALI BEGIN*/
+
+    /*ALI END*/
+
+    /*PARIYA BEGIN*/
+
+    /*PARIYA END*/
     waitKey(0);
     return 0;
 }
