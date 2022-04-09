e91a1803a3452320e217d0a2564c507f03ba5b4c
Tooraj Taraz <tooraj.info@gmail.com>
2022-04-09 21:30:35 +0430

    implementing and testing LHE without interpolation

--- utils/SerialLHE.cpp
+++ utils/SerialLHE.cpp
@@ -8,107 +8,222 @@
 const int PIXEL_RANGE = 256;
 const int MAX_PIXEL_VAL = 255;
 
-int* SerialLHE::ExtractHistogram(cv::Mat img, int* count, int x_start, int x_end, int y_start, int y_end) {   
-    int* hist = new int[PIXEL_RANGE]();
-    int height = img.size().height;
-    int width = img.size().width;
-    if (x_start < 0) x_start = 0;
-    if (x_end > height) x_end = height;
-    if (y_start < 0) y_start = 0;
-    if (y_end > width) y_end = width;
-
-    for (auto i = x_start; i < x_end; i++) {
-        for (auto j = y_start; j < y_end; j++) {
-            *count += 1;
-            hist[img.at<uchar>(i, j)]++;
+int *SerialLHE::ExtractHistogram(cv::Mat img, int *count, int x_start, int x_end, int y_start, int y_end, int *histt, int sw)
+{
+    if (histt == NULL)
+    {
+        histt = new int[PIXEL_RANGE]();
+    } else {
+        if (x_start < 0 || x_end > img.size().height || y_start < 0 || y_end > img.size().width)
+        {
+            return NULL;
         }
     }
-    return hist;
+    int height = img.size().height;
+    int width = img.size().width;
+    if (x_start < 0)
+        x_start = 0;
+    if (x_end > height)
+        x_end = height;
+    if (y_start < 0)
+        y_start = 0;
+    if (y_end > width)
+        y_end = width;
+
+    for (auto i = x_start; i < x_end; i++)
+    {
+        for (auto j = y_start; j < y_end; j++)
+        {
+            *count += sw;
+            histt[img.at<uchar>(i, j)] += sw;
+        }
+    }
+    return histt;
 }
-double* SerialLHE::CalculateProbability(int* hist, int total_pixels) {
-    double* prob = new double[PIXEL_RANGE]();
-    for (auto i = 0; i < PIXEL_RANGE; i++) {
-        prob[i] = (double) hist[i] / total_pixels;
+double *SerialLHE::CalculateProbability(int *hist, int total_pixels)
+{
+    double *prob = new double[PIXEL_RANGE]();
+    for (auto i = 0; i < PIXEL_RANGE; i++)
+    {
+        prob[i] = (double)hist[i] / total_pixels;
     }
     return prob;
 }
-int* SerialLHE::BuildLookUpTable(double* prob) {
-    int* lut = new int[PIXEL_RANGE]();
-    
-    for (auto i = 0; i < PIXEL_RANGE; i++) {
-        for (auto j = 0; j <= i; j++) {
-            lut[i] += (int) floor(prob[j] * MAX_PIXEL_VAL);
+int *SerialLHE::BuildLookUpTable(double *prob)
+{
+    int *lut = new int[PIXEL_RANGE]();
+
+    for (auto i = 0; i < PIXEL_RANGE; i++)
+    {
+        for (auto j = 0; j <= i; j++)
+        {
+            lut[i] += (int)floor(prob[j] * MAX_PIXEL_VAL);
         }
     }
     return lut;
 }
-void SerialLHE::Test(cv::Mat img) {
+void SerialLHE::Test(cv::Mat img)
+{
     int count = 0;
     int *hist = ExtractHistogram(img, &count, 0, img.size().height, 0, img.size().width);
     double *prob = CalculateProbability(hist, count);
     int *lut = BuildLookUpTable(prob);
 
-    for (auto i = 0; i < PIXEL_RANGE; i++) {
+    for (auto i = 0; i < PIXEL_RANGE; i++)
+    {
         std::cout << "i = " << i << " val = " << lut[i] << std::endl;
     }
-    //create empty base
-    cv::Mat base(img.size(), CV_8UC1, cv::Scalar(0));
-    this->ApplyLHEWithInterpol(base, img, 151);
+    // create empty base
+    //  cv::Mat base(img.size(), CV_8UC1, cv::Scalar(0));
+    //  this->ApplyLHEWithInterpol(base, img, 251);
+    //  cv::imshow("base", base);
+    //  cv::waitKey(0);
+
+    cv::Mat out(img.size().height * 0.35, img.size().width * 0.35, CV_8UC1, cv::Scalar(0));
+    cv::resize(img, out, cv::Size(), 0.35, 0.35);
+    cv::Mat base(out.size(), CV_8UC1, cv::Scalar(0));
+    this->ApplyLHE(base, out, 15);
 }
 
-void SerialLHE::ApplyLHE(cv::Mat& base, cv::Mat img, int window) {}
-
-void SerialLHE::ApplyLHEWithInterpol(cv::Mat& base, cv::Mat img, int window) {
-    std::map<std::tuple<int, int>, int*> all_luts;
-    int offset = (int) floor(window / 2.0);
+void SerialLHE::ApplyLHE(cv::Mat &base, cv::Mat img, int window)
+{
+    int offset = (int)floor(window / 2.0);
     int height = img.size().height;
     int width = img.size().width;
-    int max_i = height + ((int) floor(window / 2.0) - (height % (int) floor(window / 2.0)));
-    int max_j = width + ((int) floor(window / 2.0) - (width % (int) floor(window / 2.0)));
-    for (auto i = 0; i <= max_i; i += offset) {
-        for (auto j = 0; j <= max_j; j+= offset) {
-            std::cout << "(i, j) = (" << i << ", " << j << ")" << std::endl;
+    int count = 0;
+    int sw = 0;
+    int *hist = new int[PIXEL_RANGE]();
+    for (int i = 0; i < height; i++)
+    {
+        sw = i % 2 == 0 ? 0 : 1;
+        if (sw == 1)
+        {
+            for (int j = width - 1; j >= 0; j--)
+            {
+                if (j == (width - 1))
+                {
+                    for (int n = 0; n < window; n++)
+                    {
+                        ExtractHistogram(img, &count, i - 1 - offset, i - 1 - offset + 1, j + n - offset, j + n - offset + 1, hist, -1);
+                        ExtractHistogram(img, &count, i + window - 1 - offset, i + window - 1 - offset + 1, j + n - offset, j + n - offset + 1, hist, 1);
+                    }
+                }
+                else if (j < (width - 1))
+                {
+                    for (int n = 0; n < window; n++)
+                    {
+                        ExtractHistogram(img, &count, i + n - offset, i + n - offset + 1, j - offset, j - offset + 1, hist, 1);
+                        ExtractHistogram(img, &count, i + n - offset, i + n - offset + 1, j + window - offset, j + window - offset + 1, hist, -1);
+                    }
+                }
+                count = count > 0 ? count : 1;
+                double *prob = CalculateProbability(hist, count);
+                int *lut = BuildLookUpTable(prob);
+                base.at<uchar>(i, j) = lut[img.at<uchar>(i, j)];
+                // Clean memory
+                delete[] prob;
+                delete[] lut;
+            }
+        }
+        else
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (j == 0 && i > 0)
+                {
+                    for (int n = 0; n < window; n++)
+                    {
+                        ExtractHistogram(img, &count, i - 1 - offset, i - 1 - offset + 1, j + n - offset, j + n - offset + 1, hist, -1);
+                        ExtractHistogram(img, &count, i + window - 1 - offset, i + window - 1 - offset + 1, j + n - offset, j + n - offset + 1, hist, 1);
+                    }
+                }
+                else if (j == 0 && i == 0)
+                {
+                    for (int n = 0; n < window; n++)
+                    {
+                        for (int m = 0; m < window; m++)
+                        {
+                            ExtractHistogram(img, &count, i + n - offset, i + n - offset + 1, j + m - offset, j + m - offset + 1, hist, 1);
+                        }
+                    }
+                }
+                else if (j > 0)
+                {
+                    for (int n = 0; n < window; n++)
+                    {
+                        ExtractHistogram(img, &count, i + n - offset, i + n - offset + 1, j - 1 - offset, j - 1 - offset + 1, hist, -1);
+                        ExtractHistogram(img, &count, i + n - offset, i + n - offset + 1, j + window - 1 - offset, j + window - 1 - offset + 1, hist, 1);
+                    }
+                }
+                count = count > 0 ? count : 1;
+                double *prob = CalculateProbability(hist, count);
+                int *lut = BuildLookUpTable(prob);
+                base.at<uchar>(i, j) = lut[img.at<uchar>(i, j)];
+                // Clean memory
+                delete[] prob;
+                delete[] lut;
+            }
+        }
+    }
+    delete[] hist;
+}
+
+void SerialLHE::ApplyLHEWithInterpol(cv::Mat &base, cv::Mat img, int window)
+{
+    std::map<std::tuple<int, int>, int *> all_luts;
+    int offset = (int)floor(window / 2.0);
+    int height = img.size().height;
+    int width = img.size().width;
+    int max_i = height + ((int)floor(window / 2.0) - (height % (int)floor(window / 2.0)));
+    int max_j = width + ((int)floor(window / 2.0) - (width % (int)floor(window / 2.0)));
+    for (auto i = 0; i <= max_i; i += offset)
+    {
+        for (auto j = 0; j <= max_j; j += offset)
+        {
             int count = 0;
-            int* hist = ExtractHistogram(img, &count, i - offset, i + offset, j - offset, j + offset);
-            double* prob = CalculateProbability(hist, count);
-            int* lut = BuildLookUpTable(prob);
+            int *hist = ExtractHistogram(img, &count, i - offset, i + offset, j - offset, j + offset);
+            double *prob = CalculateProbability(hist, count);
+            int *lut = BuildLookUpTable(prob);
             all_luts[std::make_tuple(i, j)] = lut;
             delete[] hist;
             delete[] prob;
         }
     }
 
-    //Interpolating local histogram equalization
-    int padding_h = (height + ((int) floor((float) window / 2.0) - height % (int) floor((float) window / 2.0))) - height;
-    int padding_w = (width + ((int) floor((float) window / 2.0) - width % (int) floor((float) window / 2.0))) - width;
+    // Interpolating local histogram equalization
+    int padding_h = (height + ((int)floor((float)window / 2.0) - height % (int)floor((float)window / 2.0))) - height;
+    int padding_w = (width + ((int)floor((float)window / 2.0) - width % (int)floor((float)window / 2.0))) - width;
 
-    //Iterate over the image
-    for (auto i = 0; i < height; i++) {
-        for (auto j = 0; j < width; j++) {
-            int x1 = i - (i % (int) floor((float) window / 2.0));
-            int y1 = j - (j % (int) floor((float) window / 2.0));
-            int x2 = x1 + (int) floor((float) window / 2.0);
-            int y2 = y1 + (int) floor((float) window / 2.0);
+    // Iterate over the image
+    for (auto i = 0; i < height; i++)
+    {
+        for (auto j = 0; j < width; j++)
+        {
+            int x1 = i - (i % (int)floor((float)window / 2.0));
+            int y1 = j - (j % (int)floor((float)window / 2.0));
+            int x2 = x1 + (int)floor((float)window / 2.0);
+            int y2 = y1 + (int)floor((float)window / 2.0);
 
-            float x1_weight = (float) (i - x1) / (float) (x2 - x1);
-            float y1_weight = (float) (j - y1) / (float) (y2 - y1);
-            float x2_weight = (float) (x2 - i) / (float) (x2 - x1);
-            float y2_weight = (float) (y2 - j) / (float) (y2 - y1);
+            float x1_weight = (float)(i - x1) / (float)(x2 - x1);
+            float y1_weight = (float)(j - y1) / (float)(y2 - y1);
+            float x2_weight = (float)(x2 - i) / (float)(x2 - x1);
+            float y2_weight = (float)(y2 - j) / (float)(y2 - y1);
 
-            int* upper_left_lut = all_luts[std::make_tuple(x1, y1)];
-            int* upper_right_lut = all_luts[std::make_tuple(x1, y2)];
-            int* lower_left_lut = all_luts[std::make_tuple(x2, y1)];
-            int* lower_right_lut = all_luts[std::make_tuple(x2, y2)];
+            int *upper_left_lut = all_luts[std::make_tuple(x1, y1)];
+            int *upper_right_lut = all_luts[std::make_tuple(x1, y2)];
+            int *lower_left_lut = all_luts[std::make_tuple(x2, y1)];
+            int *lower_right_lut = all_luts[std::make_tuple(x2, y2)];
 
-            base.at<uchar>(i, j) = (uchar) ceil(upper_left_lut[img.at<uchar>(i, j)] * x2_weight * y2_weight +
-                                upper_right_lut[img.at<uchar>(i, j)] * x2_weight * y1_weight +
-                                lower_left_lut[img.at<uchar>(i, j)] * x1_weight * y2_weight +
-                                lower_right_lut[img.at<uchar>(i, j)] * x1_weight * y1_weight);
+            base.at<uchar>(i, j) = (uchar)ceil(upper_left_lut[img.at<uchar>(i, j)] * x2_weight * y2_weight +
+                                               upper_right_lut[img.at<uchar>(i, j)] * x2_weight * y1_weight +
+                                               lower_left_lut[img.at<uchar>(i, j)] * x1_weight * y2_weight +
+                                               lower_right_lut[img.at<uchar>(i, j)] * x1_weight * y1_weight);
         }
     }
 
-    //Cleaning all_luts
-    for (auto it = all_luts.begin(); it != all_luts.end(); it++) {
+    // Cleaning all_luts
+    for (auto it = all_luts.begin(); it != all_luts.end(); it++)
+    {
         delete[] it->second;
     }
 }
