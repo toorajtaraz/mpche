e57fc620268431eb1277cf0b18cb09bd8434de46
Tooraj Taraz <tooraj.info@gmail.com>
2022-04-15 22:09:35 +0430

    adding sections for each contributer

--- mpche.cpp
+++ mpche.cpp
@@ -2,32 +2,44 @@
 #include <omp.h>
 #include <opencv2/opencv.hpp>
 #include "headers/SerialLHE.h"
+/*TOORAJ INCLUDES BEGIN*/
+/*TOORAJ INCLUDES END*/
+
+/*ALI INCLUDES BEGIN*/
+/*ALI INCLUDES END*/
+
+/*PARIYA INCLUDES BEGIN*/
+/*PARIYA INCLUDES END*/
 using namespace cv;
+
+/*TOORAJ GLOBALS BEGIN*/
+/*TOORAJ GLOBALS END*/
+
+/*ALI GLOBALS BEGIN*/
+/*ALI GLOBALS END*/
+
+/*PARIYA GLOBALS BEGIN*/
+/*PARIYA GLOBALS END*/
+
 int main(int argc, char **argv)
 {
 #pragma omp parallel
     {
-        printf("Hello World... from thread = %d\n",
-               omp_get_thread_num());
+        omp_get_num_procs();
     }
-    std::cout << "Hello, World from non parallel part!\n";
     /*PAIR SESS START*/
-    SerialLHE slhe;
-    Mat img = imread("/home/toorajtaraz/Documents/university/MP/projects/phase1/mpche/images/he4.jpg");
-    slhe.Test(img);
     /*PAIR SESS END*/
 
     /*TOORAJ BEGIN*/
-
+    // SerialLHE slhe;
+    // Mat img = imread("/home/toorajtaraz/Documents/university/MP/projects/phase1/mpche/images/he2.jpg");
+    // slhe.Test(img);
     /*TOORAJ END*/
 
     /*ALI BEGIN*/
-
     /*ALI END*/
 
     /*PARIYA BEGIN*/
-
     /*PARIYA END*/
-    waitKey(0);
     return 0;
 }
