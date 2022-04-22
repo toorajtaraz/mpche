36f0b10eae659dd5dd3924bb12f60913aefefd96
Tooraj Taraz <tooraj.info@gmail.com>
2022-04-22 21:18:51 +0430

    minor changes

--- mpche.cpp
+++ mpche.cpp
@@ -36,29 +36,29 @@ int main(int argc, char **argv)
     /*PAIR SESS END*/
 
     /*TOORAJ BEGIN*/
-    int t ;
-    sscanf(argv[1], "%d", &t);
-    omp_set_num_threads(t);
-    ParallelLHE plhe;
-    SerialLHE slhe;
-    Mat img = imread("/home/toorajtaraz/Documents/university/MP/projects/phase1/mpche/images/he8.jpg", 0);
-    plhe.Test(img);
+    // int t ;
+    // sscanf(argv[1], "%d", &t);
+    // omp_set_num_threads(t);
+    // ParallelLHE plhe;
+    // SerialLHE slhe;
+    // Mat img = imread("/home/toorajtaraz/Documents/university/MP/projects/phase1/mpche/images/he8.jpg", 0);
+    // plhe.Test(img);
     /*TOORAJ END*/
 
     /*ALI BEGIN*/
-    for (auto a_t = 1; a_t <= 16; a_t++)
-    {
-        auto a_start = high_resolution_clock::now();
+    // for (auto a_t = 1; a_t <= 16; a_t++)
+    // {
+    //     auto a_start = high_resolution_clock::now();
 
-        omp_set_num_threads(a_t);
-        ParallelFastLHE a_pflhe;
-        Mat a_img = imread("/home/mpche/images/tree.jpg", 1);
-        a_pflhe.Test(a_img);
-        auto a_stop = high_resolution_clock::now();
-        auto a_duration = duration_cast<microseconds>(a_stop - a_start);
+    //     omp_set_num_threads(a_t);
+    //     ParallelFastLHE a_pflhe;
+    //     Mat a_img = imread("/home/mpche/images/tree.jpg", 1);
+    //     a_pflhe.Test(a_img);
+    //     auto a_stop = high_resolution_clock::now();
+    //     auto a_duration = duration_cast<microseconds>(a_stop - a_start);
 
-        std::cout << "num of threads: " << a_t << " : " << a_duration.count() / 1000 << " milliseconds" << std::endl;
-    }
+    //     std::cout << "num of threads: " << a_t << " : " << a_duration.count() / 1000 << " milliseconds" << std::endl;
+    // }
 
     /*ALI END*/
 
