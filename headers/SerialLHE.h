d73594111c213b367ff18e0c93933b1f2bbef9fb
Tooraj Taraz <tooraj.info@gmail.com>
2022-04-23 17:24:23 +0430

    removing helpers from SerialLHE

--- headers/SerialLHE.h
+++ headers/SerialLHE.h
@@ -3,12 +3,6 @@
 #include <opencv2/opencv.hpp>
 class SerialLHE
 {
-private:
-    int *ExtractHistogram(cv::Mat img, int *count, int x_start, int x_end, int y_start, int y_end, int *histt = NULL, int sw = 1);
-    int *ExtractHistogramRGB(cv::Mat img, int *count, int x_start, int x_end, int y_start, int y_end, short channel, int *histt = NULL, int sw = 1);
-    double *CalculateProbability(int *hist, int total_pixels);
-    double *BuildLookUpTable(double *prob);
-    double *BuildLookUpTableRGB(int *hist_blue, int *hist_green, int *hist_red, int count, bool free_sw = true);
 
 public:
     void Test(cv::Mat img);
